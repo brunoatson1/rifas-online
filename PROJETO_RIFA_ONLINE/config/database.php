8711220045fbe1631c4e6704ed0919d3e82c6bc6
brunoatson1 <brunoatson@gmail.com>
2025-06-17 09:46:57 -0300

    Update database.php

--- PROJETO_RIFA_ONLINE/config/database.php
+++ PROJETO_RIFA_ONLINE/config/database.php
@@ -1,19 +1,14 @@
-'mysql' => [
-    'driver' => 'mysql',
-    'url' => env('DATABASE_URL'),
-    'host' => env('DB_HOST', '127.0.0.1'),
-    'port' => env('DB_PORT', '3306'),
-    'database' => env('DB_DATABASE', 'forge'),
-    'username' => env('DB_USERNAME', 'forge'),
-    'password' => env('DB_PASSWORD', ''),
-    'unix_socket' => env('DB_SOCKET', ''),
-    'charset' => 'utf8mb4',
-    'collation' => 'utf8mb4_unicode_ci',
-    'prefix' => '',
-    'prefix_indexes' => true,
-    'strict' => true,
-    'engine' => null,
-    'options' => extension_loaded('pdo_mysql') ? array_filter([
-        PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-    ]) : [],
-],
+<?php
+// Configura conexão MySQL
+$host = "localhost";
+$db_name = "SEU_BANCO";
+$username = "SEU_USUARIO";
+$password = "SUA_SENHA";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro na conexão: " . $e->getMessage());
+}
+?>
